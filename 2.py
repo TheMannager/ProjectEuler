56fcccef60978b848a0c9823d0892dc776da8cdd
nmann20 <nathanielcmann@gmail.com>
2017-07-08 13:42:04 -0500

    first go round
    this was my first attempt to solve the problem

--- 2.py
+++ 2.py
@@ -1,4 +1,12 @@
 num1 = 1
 num2 = 2
-while num1+num2 < 1000000:
-    if num1+num
+num3
+total = 0
+while num1+num2 < 10:
+    num3 = num1 + num2
+    if num3 % 2 == 0:
+        total = total + num3
+    num1 = num2
+    num2 = num3
+    num3 = 0
+print(total)
